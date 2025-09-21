b9207f21e06da7ee900652557b1352de87a3dd2d
Lierfang Support Team <itsupport@lierfang.com>
2025-09-21 13:08:08 +0800

    Use new parse rule

--- debian/parse-machines.pl
+++ debian/parse-machines.pl
@@ -14,12 +14,20 @@ while (<STDIN>) {
 
     s/^\s+//;
     my @machine = split(/\s+/);
-    next if $machine[0] !~ m/^pc-(i440fx|q35)-(.+)$/;
-    push @$machines, {
-        'id' => $machine[0],
-        'type' => $1,
-        'version' => $2,
-    };
+    if ($machine[0] =~ m/^pc-(i440fx|q35)-(.+)$/) {
+        push @$machines, {
+            'id' => $machine[0],
+            'type' => $1,
+            'version' => $2,
+        };
+    }
+    elsif ($machine[0] =~ m/^virt-(.+)$/) {
+        push @$machines, {
+            'id' => $machine[0],
+            'type' => 'virt',
+            'version' => $1,
+        };
+    }
 }
 
 die "no QEMU machine types detected from STDIN input" if scalar (@$machines) <= 0;
